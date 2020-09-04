2e9335fceaa58707ebb3523e7ba8accff0551c8a
孔佳齐丶 <kjqjava@163.com>
2020-09-04 08:01:28 +0800

    Initial commit

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,49 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-02-24
+ */
+@Api(description="讲师管理")
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+    //访问地址： http://localhost:8001/eduservice/teacher/findAll
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1 查询讲师表所有数据
+    //rest风格
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("findAll")
+    public R findAllTeacher() {
+        //调用service的方法实现查询所有的操作
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items",list);
+    }
+}
+
